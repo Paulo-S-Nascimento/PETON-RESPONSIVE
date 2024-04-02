ee637b542f861d21c2a0882c59ff38c379abd25f
Paulo Sergio Nascimento Gomes <37022263@sempreunijuazeiro.com.br>
2024-04-01 23:43:31 -0300

    adicionando pastal

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var botao = document.getElementsByClassName('button-1')[0];
+botao.onclick = function(){
+    alert('Obrigado por entrar na nossa lista de espera');
+}
+
+
+var botao = document.getElementsByClassName('button-2')[0];
+botao.onclick = function(){
+    alert('Voê já está na lista de espera');
+}
